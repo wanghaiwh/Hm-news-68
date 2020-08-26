acb830203c70932624590f2913b6158e5148928a
海王 <1540879214@qq.com>
2020-08-26 18:45:32 +0800

    配置了项目中的vant

--- src/main.js
+++ src/main.js
@@ -2,8 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-Vue.config.productionTip = false
+// 导入通用的样式
+import './styles/base.less'
+// 导入字体样式
+import './styles/iconfont.css'
 
+// 导入Vant
+// import Vant from 'vant'
+import { Button } from 'vant'
+// import 'vant/lib/index.css'
+// console.log(Vant)
+// 全局将vantage所有组件都导入
+// Vue.use(Vant)
+Vue.use(Button)
+
+Vue.config.productionTip = false
 new Vue({
   router,
   render: h => h(App)
